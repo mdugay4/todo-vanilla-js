006ec17750d43350cf16443a45c1870aa1b79d48
Mark Dugay <mdugay4@gmail.com>
2023-01-25 20:32:52 -0800

    Todo add item functionality

---
+++ app.js
@@ -0,0 +1,35 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+// Functions
+function addTodo(event) {
+    // Prevent form from submitting
+    event.preventDefault();
+    // Todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todo');
+    // Create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    // Check button
+    const checkButton = document.createElement('button');
+    checkButton.innerHTML = '<i class="fas fa-check"></i>';
+    checkButton.classList.add('check-btn');
+    todoDiv.appendChild(checkButton);
+    // Trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    // Append to list
+    todoList.appendChild(todoDiv);
+    // Clear todo value
+    todoInput.value = '';
+}
