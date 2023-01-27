7f7c920cc44087b5e9fea858c704ee41ae749e32
Mark Dugay <mdugay4@gmail.com>
2023-01-26 22:16:25 -0800

    Add/remove items and filter items

--- app.js
+++ app.js
@@ -2,9 +2,12 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector('.filter-todo');
 
 // Event Listeners
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 // Functions
 function addTodo(event) {
@@ -32,4 +35,57 @@ function addTodo(event) {
     todoList.appendChild(todoDiv);
     // Clear todo value
     todoInput.value = '';
+}
+
+function deleteCheck(event) {
+    console.log(event.target);
+    const item = event.target;
+    // Delete todo
+    console.log(item.classList);
+    if (item.classList[0] === 'trash-btn') {
+        const todo = item.parentElement;
+        // Delete animation
+        todo.classList.add('fall');
+        // todo.remove();
+        todo.addEventListener('transitionend', function () {
+            todo.remove();
+        });
+    }
+    // Check todo
+    if (item.classList[0] === 'check-btn') {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+}
+
+function filterTodo(event) {
+    const todos = todoList.childNodes;
+    console.log(todos);
+    todos.forEach(function (todo) {
+        switch (event.target.value) {
+            case 'all':
+                todo.style.display = 'flex';
+                // console.log('all filter selected');
+                break;
+            case 'completed':
+                if (todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                // console.log('completed filter selected');
+                break;
+            case 'uncompleted':
+                if (!todo.classList.contains('completed')) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                // console.log('uncompleted filter selected');
+                break;
+            default:
+                // console.log('Hello this is default');
+                break;
+        }
+    });
 }
